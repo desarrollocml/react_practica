f6cf7ebf2854e5343780cc1d32ad3ec3cd78d6ea
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-02 20:52:36 -0300

    todos anadir eliminar local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+import React, {useState, useRef,useEffect} from "react";
+import {v4 as uuidv4} from "uuid";
+import {TodoList} from "./componentes/TodoList";
 
+const KEY ="todoApp.todos";
 
-function App() {
-  return (
-    <div >
-      <h1>hola</h1>
-    </div>
+export default function App(){
+  
+  const todoTaskRef = useRef();
+
+  const [todos, setTodos] = useState([
+    {id:1, task:"Tarea", completed: false},
+  ]);
+
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(KEY));
+    if(storedTodos){
+      setTodos(storedTodos);
+    }
+  },[]);
+
+  useEffect(()=>{
+    localStorage.setItem(KEY, JSON.stringify(todos));
+  },[todos]);
+
+  const toggleTodo = (id)=>{
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo)=> todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleTodoAdd = (e) =>{
+    const task = todoTaskRef.current.value;
+    if(task === "") return;
+
+    setTodos((prevTodos)=>{
+      return [...prevTodos, {id: uuidv4(), task, completed: false}];
+    });
+    todoTaskRef.current.value = null;
+  };
+
+  const handleClearAll = ()=>{
+    const newTodos = todos.filter((todo)=> !todo.completed);
+    setTodos(newTodos);
+  };
+   return (
+    < >
+      <TodoList todos = {todos} toggleTodo={toggleTodo}/>
+      <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"/>
+      <button onClick={handleTodoAdd}>Añadir</button>
+      <button onClick={handleClearAll}>Eliminar</button>
+      <div>
+        Te quedan {todos.filter((todo) => !todo.completed).length}
+        tareas por terminar
+      </div>
+    </>
   );
 }
-
-export default App;
